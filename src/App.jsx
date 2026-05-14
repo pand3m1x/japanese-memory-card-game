7a86f839195a3c4239eeed25dfc6eccd2daf5959
TheRedQueen <pand3m1x@gmail.com>
2026-05-14 17:41:22 -0400

    Card will alert when matched (first only) and will reset cards

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ const cardValues = [
 function App() {
 
   const [cards, setCards] = useState([]);
+  const [flippedCards, setFlippedCards] = useState([]);
 
   const initializeGame = () => {
     // SHUFFLE THE CARDS
@@ -61,7 +62,30 @@ function App() {
         return c;
       }
     });
-    setCards(newCards)
+    setCards(newCards);
+
+    const newFlippedCards = [...flippedCards, card.id]
+    setFlippedCards(newFlippedCards);
+
+    //check for match if two cards are flipped
+    if (flippedCards.length === 1) {
+      const firstCard = cards[flippedCards[0]]
+      if (firstCard.value === card.value) {
+        alert("Match!")
+      } else {
+
+        //flip back card 1, card 2
+
+        const flippedBackCard = newCards.map((c) => {
+          if (newFlippedCards.includes(c.id || c.id === card.id)) {
+            return {...c,isFlipped: false };
+          } else {
+            return c;
+          }
+        });
+        setCards(flippedBackCard);
+      }
+    }
   };
 
   return (
