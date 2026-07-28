b0e51575719b4fe10ff615a8fdbd386f10c452a5
TheRedQueen <pand3m1x@gmail.com>
2026-07-27 22:04:26 -0400

    added clarifying categories for sea animals, added emojis to aniamls and sea animals

---
+++ src/vocab-categories/seaAnimalsCategory.js
@@ -0,0 +1,16 @@
+export const landAnimalsCategory = [ 
+
+{id:"c34", emoji: "🦑", wordJP:"じゅず"},
+{id:"c28", emoji: "🐙", wordJP:"リュックサック"},
+{id:"c29", emoji: "🦐", wordJP:"したぎ"},
+{id:"c30", emoji: "🦞", wordJP:"さいふ"},
+{id:"c31", emoji: "🦀", wordJP:"ボウタイ"},
+{id:"c32", emoji: "🐡", wordJP:"ヘッド・フォン"},
+{id:"c33", emoji: "🐠", wordJP:"うでどけい"},
+{id:"c34", emoji: "🐟", wordJP:"じゅず"},
+{id:"c34", emoji: "🐬", wordJP:"じゅず"},
+{id:"c28", emoji: "🐳", wordJP:"リュックサック"},
+{id:"c29", emoji: "🦈", wordJP:"したぎ"},
+{id:"c30", emoji: "🐚", wordJP:"さいふ"},
+{id:"c31", emoji: "🦀", wordJP:"ボウタイ"},
+]
