93cee2def75df45acdad0cd716ac465bfe1c9a58
TheRedQueen <pand3m1x@gmail.com>
2026-07-28 20:22:41 -0400

    added new categories, seperated birds and insects from land animals, added travel

---
+++ src/vocab-categories/birdsInsectsCategory.js
@@ -0,0 +1,19 @@
+
+export const birdsInsectsCategory = [
+
+{id:"c27", emoji: "🐝", wordJP:"てぶくろ"},
+{id:"c28", emoji: "🐛", wordJP:"リュックサック"},
+{id:"c29", emoji: "🦋", wordJP:"したぎ"},
+{id:"c30", emoji: "🐌", wordJP:"さいふ"},
+{id:"c31", emoji: "🐞", wordJP:"ボウタイ"},
+{id:"c32", emoji: "🐜", wordJP:"ヘッド・フォン"},
+{id:"c33", emoji: "🦟", wordJP:"うでどけい"},
+{id:"c34", emoji: "🦗", wordJP:"じゅず"},
+{id:"c28", emoji: "🕷", wordJP:"リュックサック"},
+{id:"c29", emoji: "🕸", wordJP:"したぎ"},
+{id:"c30", emoji: "🦂", wordJP:"さいふ"},
+{id:"c31", emoji: "🐢", wordJP:"ボウタイ"},
+{id:"c32", emoji: "🐍", wordJP:"ヘッド・フォン"},
+{id:"c33", emoji: "🦎", wordJP:"うでどけい"},
+{id:"c34", emoji: "🦖", wordJP:"じゅず"},
+]
