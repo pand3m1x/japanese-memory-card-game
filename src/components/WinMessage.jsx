29aadad334ee4da3011b5be29851e1490fd2add9
TheRedQueen <pand3m1x@gmail.com>
2026-08-05 14:31:12 -0400

    winning message is now a toast, two buttons added, needs lofic

--- src/components/WinMessage.jsx
+++ src/components/WinMessage.jsx
@@ -4,6 +4,10 @@ export const WinMessage = ({ moves }) => {
     <div className="win-message">
       <h2>Congrats!</h2>
       <p>You completed the game in {moves} moves!</p>
+      <div>
+        <button>Play Again?</button>
+        <button>Pick New Category?</button>
+      </div>
     </div>
   );
 };
